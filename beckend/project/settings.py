46f3e04e88f0b4edc0b121bbb490a1f9b108b268
Muhammadaziz <muhammadazizdev@gmail.com>
2024-10-20 12:46:46 +0600

    add cors allowed origins

--- beckend/project/settings.py
+++ beckend/project/settings.py
@@ -160,6 +160,10 @@ APPEND_SLASH = False
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    'https://homenest-pn5n.onrender.com',
+]
+
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
